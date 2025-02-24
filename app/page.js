880fdfe5586a0d90b31790a326259efea80a5c85
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-02-24 19:17:12 +0530

    Updated Files

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/ui/hero";
 import Seo from "@/components/ui/seo";
 export default function Home() {
   return (
-    <section className="flex flex-col bg-gradient-to-b from-black to-black/80">
+    <section className="flex flex-col">
       <Header/>
       <Hero/>
       <Seo/>
--- components/ui/header.js
+++ components/ui/header.js
@@ -1,31 +1,66 @@
-import Image from "next/image";
-import logo from '../../public/logo.png';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Settings } from "lucide-react";
+"use client";
+import React from "react";
+import userSvg from "../../public/icons/xmark.svg";
+import xmark from "../../public/icons/xmark.svg";
+import bars from "../../public/icons/xmark.svg";
+import Link from "next/link";
 
-function Header(props) {
+export default function Header() {
+  function displayNavbar() {
+    document.getElementById("navbar").classList.remove("-right-full");
+  }
+
+  function closeNavbar() {
+    document.getElementById("navbar").classList.add("-right-full");
+  }
+
   return (
-    <header className="flex flex-row items-center justify-between section-p gap-10">
-      <div className="flex items-center gap-2">
-        <Image src={logo} width={50} height={50} className="cursor" alt="logo"/>
-        <h2 className="font-bold text-[1.7rem]">FootPrintly</h2>
+    <header className="flex items-center justify-center gap-20 fixed top-0 left-0 w-full shadow-sm z-50 flex px-6 py-4 section-p">
+      {/* Logo */}
+      <div className="flex items-center gap-4">
+        {/* <img src={logo} alt="Logo" className="w-32 h-14 object-cover" /> */}
+        <h1 className="text-xl font-bold tracking-widest cursor-pointer">
+          FootPrintly
+        </h1>
       </div>
-      <DropdownMenu className="border-none outline-none ">
-        <DropdownMenuTrigger className="py-[4px] focus:outline-none"><Settings/> </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      {/* Navbar */}
+      <ul
+        id="navbar"
+        className="flex items-center gap-6 transition-all duration-300 absolute md:static bg-white md:bg-transparent right-0 -right-full top-0 h-screen md:h-auto w-64 md:w-auto md:flex-row flex-col shadow-lg md:shadow-none p-6 md:p-0"
+      >
+        <li className="md:hidden cursor-pointer" onClick={closeNavbar}>
+          <img src={xmark} alt="Close navbar" />
+        </li>
+        <li>
+          <Link href={"/"} className="text-gray-900 font-medium">
+            Start
+          </Link>
+        </li>
+        <li>
+          <Link href={"/"} className="text-gray-900 font-medium">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href={"/"} className="text-gray-900 font-medium">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link href={"/"} className="text-gray-900 font-medium">
+            Contact
+          </Link>
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer">
+          <img src={userSvg} className="w-5 h-5" alt="Logout" />
+          <Link href={"/"} className="text-gray-900 font-medium">
+            Log Out
+          </Link>
+        </li>
+      </ul>
+      <div className="md:hidden cursor-pointer" onClick={displayNavbar}>
+        <img src={bars} alt="Menu" />
+      </div>
     </header>
   );
 }
-
-export default Header;
