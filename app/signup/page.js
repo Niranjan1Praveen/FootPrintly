415b2a3afb5828796f4f28ddef25e8d35c2f2699
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-02-26 16:57:27 +0530

    Updated Files

--- app/signup/page.js
+++ app/signup/page.js
@@ -6,10 +6,11 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
-import logo from "../../public/logo/logo.png"
+import logo from "../../public/logo/logo.png";
 
 function Page() {
   const [error, setError] = useState(false);
+  const [isBounce, setIsBounce] = useState(false);
   const router = useRouter();
   const [formData, setFormData] = useState({
     username: "",
@@ -17,7 +18,13 @@ function Page() {
     email: "",
     authToken: "",
   });
+  const handleFocus = () => {
+    setIsBounce(true);
+  };
 
+  const handleBlur = () => {
+    setIsBounce(false);
+  };
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -58,14 +65,21 @@ function Page() {
   };
 
   return (
-    <section className="flex flex-col justify-center gap-6 h-screen section-p">
+    <section className="flex flex-col gap-6 section-p min-h-screen">
       {/* SignUp Content */}
-      <div className="flex gap-10 justify-between">
-        <div className="flex flex-col justify-between gap-5">
-          <h2 className="text-2xl font-bold">Get Started with FootPrintly</h2>
-          <p>The easiest way to get icons on your website is with a Kit. It's your very own custom version of Font Awesome, all bundled up with only the icons, tools, and settings you need.</p>
+      <div className="flex gap-10 items-center flex-wrap">
+        <div className="flex flex-col gap-5 max-w-[600px]">
+          <h2 className="text-2xl font-bold text-[#1CB0F6]">Get Started with FootPrintly</h2>
+          <p>
+            The easiest way to get icons on your website is with a Kit. It's
+            your very own custom version of Font Awesome, all bundled up with
+            only the icons, tools, and settings you need.
+          </p>
         </div>
-        <Image href={logo} alt="bounce logo icon" className="w-full h-full"/>
+        <div className="flex items-center">
+          <Image src={logo} alt="bounce logo icon" width={200} height={200} className={isBounce ? 'animate-bounce' : ''}/>
+          <quote className="italic border-pl-4 max-w-[200px]">Getting values in those fields makes me happy!</quote>
+        </div>
       </div>
       <div className="flex flex-col gap-4 px-8 py-10 bg-[var(--secondary-background)] rounded-xl shadow-md">
         {error && (
@@ -81,6 +95,8 @@ function Page() {
           placeholder="Enter your username"
           value={formData.username}
           onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           className="bg-gray-200 text-gray-900 border border-gray-300"
         />
 
@@ -91,6 +107,8 @@ function Page() {
           placeholder="Enter your email"
           value={formData.email}
           onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           className="bg-gray-200 text-gray-900 border border-gray-300"
         />
 
@@ -101,21 +119,28 @@ function Page() {
           placeholder="Create a password"
           value={formData.password}
           onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           className="bg-gray-200 text-gray-900 border border-gray-300"
         />
 
         <Button
-          className="bg-[#1ed760] transform transition-all duration-300 hover:scale-105 text-white font-semibold mt-4 py-2 rounded-lg"
+          className="w-[100px] bg-[#1ed760] transform transition-all duration-300 hover:scale-105 text-white mt-4 py-2 rounded-full border-none"
           onClick={handleSignup}
         >
           Sign Up
         </Button>
-
-        <p className="text-center text-sm text-gray-600 mt-4">
-          Already have an account?
-          <Link href="/login" className="underline text-[#1ed760] hover:text-green-400"> Login to FootPrintly</Link>
-        </p>
       </div>
+      <p className="text-center text-sm text-gray-600 mt-4">
+        Already have an account?
+        <Link
+          href="/login"
+          className="underline text-[#1ed760] hover:text-green-600"
+        >
+          {" "}
+          Login to FootPrintly
+        </Link>
+      </p>
     </section>
   );
 }
--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import xmark from "../public/icons/xmark.svg";
 import bars from "../public/icons/bars.svg";
 import signIn from "../public/icons/sign-in.svg";
@@ -8,16 +8,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
-  function displayNavbar() {
-    document.getElementById("navbar").style.right = "0";
-  }
-
-  function closeNavbar() {
-    document.getElementById("navbar").style.right = "-100%";
-  }
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="flex items-center justify-center gap-20 top-0 left-0 w-full shadow-sm z-50 flex px-6 py-4 section-p">
+    <header className="flex items-center justify-between py-4 w-full shadow-sm z-50 section-p">
       {/* Logo */}
       <div className="flex items-center gap-4">
         <Image src={logo} alt="Logo" width={30} height={30} />
@@ -25,13 +19,15 @@ export default function Header() {
           FootPrintly
         </h1>
       </div>
-      {/* Navbar */}
+
       <ul
         id="navbar"
-        className="flex items-center gap-6 transition-all duration-300 overflow-hidden h-screen absolute md:static bg-white md:bg-transparent right-0 top-0 md:h-auto w-64 md:w-auto md:flex-row flex-col shadow-lg md:shadow-none p-6 md:p-0"
+        className={`fixed top-0 right-0 h-screen w-64 bg-white shadow-lg p-6 flex flex-col gap-6 transition-transform duration-300 
+        ${isOpen ? "translate-x-0" : "translate-x-full"} 
+        md:static md:h-auto md:w-auto md:bg-transparent md:flex-row md:shadow-none md:translate-x-0`}
       >
-        <li className="md:hidden cursor-pointer" onClick={closeNavbar}>
-          <Image src={xmark} alt="Close navbar" className="icon" />
+        <li className="md:hidden cursor-pointer" onClick={() => setIsOpen(false)}>
+          <Image src={xmark} alt="Close navbar" className="w-6 h-6" />
         </li>
         <li>
           <Link href={"/"} className="text-gray-900 font-medium">
@@ -52,8 +48,10 @@ export default function Header() {
           <Image src={signIn} className="w-5 h-5" alt="Logout" />
         </li>
       </ul>
-      <div className="md:hidden cursor-pointer" onClick={displayNavbar}>
-        <Image src={bars} alt="Close navbar" className="icon" />
+
+      {/* Mobile Menu Icon */}
+      <div className="md:hidden cursor-pointer" onClick={() => setIsOpen(true)}>
+        <Image src={bars} alt="Open navbar" className="w-6 h-6" />
       </div>
     </header>
   );
